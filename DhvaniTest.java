9b4dbf72f22573d80f10c416551f1f327d86bff9
vivekbhalala <vivekbhalala91@gmail.com>
2019-02-27 21:23:07 -0800

    Create DhvaniTest.java

---
+++ DhvaniTest.java
@@ -0,0 +1,38 @@
+import java.util.HashSet;
+import java.util.Random;
+import java.util.Scanner;
+import java.util.Set;
+
+public class GussingGame {
+
+	public static void main(String[] args) {
+		int rangeLimit = 100;
+		Random randomNumber = new Random();
+		int cValue = randomNumber.nextInt(rangeLimit);
+		Scanner sc = new Scanner(System.in);
+		//Using set to not count same attempts multiple times
+		Set<Integer> set = new HashSet<Integer>();
+		while (true) {
+			System.out.println("Please enter number between 0 to 100");
+			int gValue = sc.nextInt();
+			if (gValue < 0 || gValue > rangeLimit) {
+				System.out.println("Invalid input");
+			} else if (gValue == cValue) {
+				set.add(gValue);
+				System.out.println("You Guessed it in " + set.size() + " steps");
+				break;
+			} else if (gValue < cValue) {
+				set.add(gValue);
+				System.out.println(gValue+" too small");
+			} else if (gValue > cValue) {
+				set.add(gValue);
+				System.out.println(gValue+" too large");
+			}
+
+		}
+
+		sc.close();
+
+	}
+
+}
